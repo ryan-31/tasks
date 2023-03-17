6a77e5823c24ff14fc5057c3526de6398de00217
Ryan Sanchez <ryansan@udel.edu>
2023-03-17 00:03:15 -0400

    completed MultipleChoiceQuestion

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,28 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [selectedOption, setSelectedOption] = useState<string>(options[0]);
+
+    function updateSelected(event: React.ChangeEvent<HTMLSelectElement>) {
+        setSelectedOption(event.target.value);
+    }
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="mcq">
+                <Form.Label>What is the answer?</Form.Label>
+                <Form.Select value={selectedOption} onChange={updateSelected}>
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+
+            {selectedOption === expectedAnswer && <div>✔️</div>}
+            {selectedOption !== expectedAnswer && <div>❌</div>}
         </div>
     );
 }
