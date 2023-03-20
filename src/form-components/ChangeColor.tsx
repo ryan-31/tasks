8d019dfc9a520f7a54e84d157bc3808236e7f21b
Ryan Sanchez <ryansan@udel.edu>
2023-03-19 23:31:22 -0400

    completed ChangeColor

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,48 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function ChangeColor(): JSX.Element {
+    const colors = [
+        "red",
+        "green",
+        "blue",
+        "orange",
+        "purple",
+        "pink",
+        "yellow",
+        "grey"
+    ];
+    const [chosenColor, setChosenColor] = useState<string>(colors[0]);
+
+    function updateChosenColor(event: React.ChangeEvent<HTMLInputElement>) {
+        setChosenColor(event.target.value);
+    }
+
     return (
         <div>
             <h3>Change Color</h3>
+            {colors.map((color) => (
+                <Form.Check
+                    inline
+                    type="radio"
+                    key={color}
+                    label={color}
+                    value={color}
+                    checked={chosenColor === color}
+                    onChange={updateChosenColor}
+                />
+            ))}
+
+            <div
+                data-testid="colored-box"
+                style={{
+                    backgroundColor: chosenColor,
+                    height: "30px",
+                    width: "50px"
+                }}
+            >
+                {chosenColor}
+            </div>
         </div>
     );
 }
